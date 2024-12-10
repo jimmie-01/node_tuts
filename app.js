a42f9212b76abaf400bc054b5bc52db8f8be783e
Fakile Razaq <razaqjimmie91@gmail.com>
2024-12-10 17:50:16 +0100

    Delete  and AJAX request

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.post('/blogs', (req, res) => {
 
 app.get('/blogs/:id', (req, res) => {
 	const id = req.params.id;
+
 	Blog.findById(id)
 	  .then(result => {
 		res.render('details', { blog: result, title: 'Blog Details' });
@@ -64,6 +65,17 @@ app.get('/blogs/:id', (req, res) => {
 		console.log(err);
 	  });
   });
+
+app.delete('/blogs/:id', (req, res) => {
+	const id = req.params.id;
+
+	Blog.findByIdAndDelete(id)
+		.then(result => {
+			res.json({ redirect: '/blogs'});
+		}).catch(err => {
+			console.log(err);
+		});
+})
 
 app.use((req, res) => {
 	res.status(404).render('404', { title: '404'});

8146b7980fbd4e5355513c380de6ee054032be1e
Fakile Razaq <razaqjimmie91@gmail.com>
2024-12-10 16:58:00 +0100

    Created Blog body EJS

--- app.js
+++ app.js
@@ -22,47 +22,49 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.get('/', (req, res) => {
-	
 	res.redirect('/blogs');
-
 });
 
-app.get('/about', (req,res) => {
-
+app.get('/about', (req, res) => {
 	res.render('about', { title: 'About' });
-
-})
-
-//blog routes
-app.get('/blogs', (req, res) => {
-	// sort blogs in descending order
-	Blog.find().sort({ createdAt: -1 })
-	.then((result) => {
-		res.render('index', { title: 'All-Blogs', blogs: result });
-	}).catch((err) => {
-		console.log(err);
-	});
 });
 
-app.post('/blogs', (req, res) => {
-	const blog = new Blog(req.body);
+// blog route
+app.get('/blogs/create', (req, res) => {
+	res.render('create', { title: 'Create a new blog' });
+});
 
-	blog.save()
-		.then((result) => {
-			res.redirect('/blogs');
-		}).catch((err) => {
+app.get('/blogs', (req, res) => {
+	Blog.find().sort({ createdAt: -1})
+		.then(result => {
+			res.render('index', { title: 'All Blogs', blogs: result });
+		}).catch(err => {
 			console.log(err);
 		});
 });
 
-app.get('/blogs/create', (req, res) => {
-
-	res.render('create', { title: 'Create New Blog'});
-
+app.post('/blogs', (req, res) => {
+	const blog = new Blog(req.body);
+	
+	blog.save()
+		.then(result => {
+			res.redirect('/blogs');
+		}).catch(err => {
+			console.log(err);
+		})
 })
+
+app.get('/blogs/:id', (req, res) => {
+	const id = req.params.id;
+	Blog.findById(id)
+	  .then(result => {
+		res.render('details', { blog: result, title: 'Blog Details' });
+	  })
+	  .catch(err => {
+		console.log(err);
+	  });
+  });
 
 app.use((req, res) => {
-
 	res.status(404).render('404', { title: '404'});
-
-})
+});
